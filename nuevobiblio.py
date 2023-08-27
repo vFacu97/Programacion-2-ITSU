e1d579c4ed09693b40e6b0767d9f900922070199
Ruben Grauberger <113745540+rubegrau@users.noreply.github.com>
2023-08-27 00:03:48 -0300

    Add files via upload
    cambios en el menu de edicion

--- nuevobiblio.py
+++ nuevobiblio.py
@@ -19,16 +19,36 @@ class Autores:
 
     def buscar_autor(self, nombre_buscar):
         for autor in self.autores:
-            if autor['Nombres'] == nombre_buscar:
+            if autor['Nombres'] or autor['Apellido'] == nombre_buscar:
                 return autor
         return None
     
     def editar_autor(self, autor_encontrado):
-        nuevo_nombre = input("Nuevo nombre: ")
-        nuevo_apellido = input("Nuevo apellido: ")
-        autor_encontrado['Nombres'] = nuevo_nombre
-        autor_encontrado['Apellido'] = nuevo_apellido
-        print("Autor modificado.")
+        print("1. Modificar nombre del autor")
+        print("2. Modificar apellido del autor")
+        print("3. Modificar ambos")
+        print("4. Salir")
+        seleccion = int(input("Seleccionar una opción: "))
+        if seleccion == 1:
+            nuevo_nombre = input("Ingrese el nuevo nombre: ")
+            autor_encontrado['Nombres'] = nuevo_nombre
+            print("Nombre modificado")
+        elif seleccion == 2:
+            nuevo_apellido = input("Ingrese el nuevo apellido: ")
+            autor_encontrado['Apellido'] = nuevo_apellido
+            print("Apellido modificado")
+        elif seleccion == 3:            
+            nuevo_nombre = input("Nuevo nombre: ")
+            nuevo_apellido = input("Nuevo apellido: ")
+            autor_encontrado['Nombres'] = nuevo_nombre
+            autor_encontrado['Apellido'] = nuevo_apellido
+            print("El nombre completo fue modificado.")
+        elif seleccion == 4:
+            print("")
+        else:
+            print("seleccion incorrecta, intente nuevamente")
+            
+            
     
 autor = Autores()
 
@@ -45,7 +65,7 @@ while True:
     elif opcion == 2:
         autor.mostrarautor()
     elif opcion == 3:
-        nombre_buscar = input("Ingrese el nombre del autor a buscar y editar: ")
+        nombre_buscar = input("Ingrese el nombre o apellido del autor a buscar y editar: ")
         autor_encontrado = autor.buscar_autor(nombre_buscar)
         if autor_encontrado:
             autor.editar_autor(autor_encontrado)
