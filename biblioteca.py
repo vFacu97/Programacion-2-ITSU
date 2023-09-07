1ed6fd3b8608c07a9a195c45bedba2d82ba4b895
Facundo <vfacu44@gmail.com>
2023-09-07 18:12:45 -0300

    Agregando funcionalidad a menu de libros

--- biblioteca.py
+++ biblioteca.py
@@ -44,7 +44,13 @@ class Libros:
                     elif opcion == 4:
                         condicion = True
 #-----------Fin de la clase libros--------------
-
+#-----------Comienzo clase biblioteca-----------
+class Biblioteca(Libros):
+    def mostrar_libros(self):
+            for muestra in self.libros:
+                print("Titulo: ", muestra['titulo'])
+                print("Año: ", muestra['anio'])
+                print("Autor ", muestra['autor'])
 
 #---------------comienzo de clase de autores----------------
 class Autores:
@@ -96,32 +102,100 @@ class Autores:
             print("")
         else:
             print("seleccion incorrecta, intente nuevamente")
-            
-            
     
-autor = Autores()
-
-while True:
-    print("1. Agregar autor")
-    print("2. Mostrar autores")
-    print("3. Buscar y editar autor")
-    print("4. Salir")
-    
-    opcion = int(input("Seleccionar una opción: "))
-    
-    if opcion == 1:
-        autor.agregarautor()
-    elif opcion == 2:
-        autor.mostrarautor()
-    elif opcion == 3:
-        nombre_buscar = input("Ingrese el nombre o apellido del autor a buscar y editar: ")
-        autor_encontrado = autor.buscar_autor(nombre_buscar)
-        if autor_encontrado:
-            autor.editar_autor(autor_encontrado)
-        else:
-            print("Autor no encontrado.")
-    elif opcion == 4:
-        break
-    else:
-        print("Opción inválida. Por favor, seleccione una opción válida.")
 #-------------------Fin de la clase autores--------------
+
+#--------------inicio de menu-----------------------
+class Menu(Biblioteca,Autores):
+    def menu_principal(self):
+        print("""
+                **********
+                   MENÚ
+                **********
+                1) Libros
+                2) Autores
+                3) Biblioteca
+                4) Salir
+            """)
+        self.opciones()
+        
+    def opciones(self):
+        fin=False
+        while not (fin):
+            op = int(input("Ingrese el número de la opción deseada: "))
+            if op == 1:
+                self.menu_libros()
+            elif op == 2:
+                self.menu_autores()
+            elif op == 3:
+                self.menu_biblioteca()
+            elif op == 4:
+                exit()
+        
+    def menu_libros(self):
+        print("""
+                **********
+                  LIBROS
+                **********
+                1) Agregar libro
+                2) Editar libro
+                3) Mostrar libros
+                4) Salir
+            """)
+        fin = False
+        while not(fin):
+            op = int(input("Ingrese el número de la opción deseada: "))
+            if op == 1:
+                print("Agregando...")
+                self.agregar_libro()
+                self.menu_libros()
+            elif op == 2:
+                print("Editando...")
+                self.editar_libro()
+                self.menu_libros()
+            elif op == 3:
+                print("Mostrando...")
+                self.mostrar_libros()
+                self.menu_libros()
+            elif op == 4:
+                self.menu_principal()
+                
+    def menu_autores(self):
+        print("""
+                **********
+                  AUTORES
+                **********
+                1) Agregar autor
+                2) Editar autor
+                3) Mostrar autor
+                4) Salir
+            """)
+        fin = False
+        while not(fin):
+            op = int(input("Ingrese el número de la opción deseada: "))
+            if op == 1:
+                print("Agregando...")
+            elif op == 2:
+                print("Editando...")
+            elif op == 3:
+                print("Mostrando...")
+            elif op == 4:
+                self.menu_principal()
+                
+    def menu_biblioteca(self):
+        print("""
+                ************
+                 BIBLIOTECA
+                ************
+                1) Mostrar
+                2) Salir
+            """)
+        fin = False
+        while not(fin):
+            op = int(input("Ingrese el número de la opción deseada: "))
+            if op == 1:
+                print("Mostrando")
+            elif op == 2:
+                self.menu_principal()
+menu = Menu()
+menu.menu_principal()
